ea1839fae68dab3511bebc5d73d8662754628539
bodav <bodavidsen@gmail.com>
2024-12-29 19:56:29 +0100

    env vars

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,9 +5,8 @@ import Logo from "@/assets/app.svg?react";
 
 function Navbar() {
   const appName = import.meta.env.VITE_APP_NAME;
-  const sha = import.meta.env.VITE_GIT_SHA.substring(0, 7);
   const repo = import.meta.env.VITE_GITHUB_REPOSITORY;
-  const ref = import.meta.env.VITE_GIT_BRANCH_NAME;
+  const ref = import.meta.env.VITE_GIT_REF.split("/").pop() || "v0.0";
 
   return (
     <div className="bg-secondary text-muted-foreground border-b h-14 px-3 flex justify-between">
@@ -27,7 +26,7 @@ function Navbar() {
       <div className="inline-flex items-center">
         <div className="self-end mr-1 pb-3">
           <span className="text-xs">
-            {repo}/{ref}/{sha}
+            {repo}/{ref}
           </span>
         </div>
         <GithubButton />
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,9 +4,7 @@
 interface ImportMetaEnv {
   readonly VITE_GITHUB_REPOSITORY: string;
   readonly VITE_APP_NAME: string;
-  readonly VITE_GIT_SHA: string;
-  readonly VITE_GIT_BRANCH_NAME: string;
-  readonly VITE_EXPOSED: string;
+  readonly VITE_GIT_REF: string;
 }
 
 interface ImportMeta {
