ad54589514df0ad8c7683cc790151d7b00c38094
bodav <bodavidsen@gmail.com>
2025-01-03 20:12:03 +0100

    fixed power missing

--- src/lib/parser.ts
+++ src/lib/parser.ts
@@ -33,7 +33,8 @@ function parseGPX(gpxString: string): GPXTrack {
   const parser = new DOMParser();
   const xmlDoc = parser.parseFromString(gpxString, "application/xml");
 
-  const timeString = xmlDoc.getElementsByTagName("time")[0]?.textContent || undefined;
+  const timeString =
+    xmlDoc.getElementsByTagName("time")[0]?.textContent || undefined;
   const time = timeString ? new Date(timeString) : undefined;
   const name = xmlDoc.getElementsByTagName("name")[0]?.textContent || "";
   const type = xmlDoc.getElementsByTagName("type")[0]?.textContent || "";
@@ -43,29 +44,39 @@ function parseGPX(gpxString: string): GPXTrack {
 
   for (let i = 0; i < trkpts.length; i++) {
     const trkpt = trkpts[i];
-    const lat = parseFloat(trkpt.getAttribute("lat") || "0");
-    const lon = parseFloat(trkpt.getAttribute("lon") || "0");
-    const timeString =
-      trkpt.getElementsByTagName("time")[0]?.textContent || null;
-    const time = timeString ? new Date(timeString) : new Date(0);
-    const ele = parseFloat(
-      trkpt.getElementsByTagName("ele")[0].textContent || "0"
-    );
-    const extensions = trkpt.getElementsByTagName("extensions")[0];
-    const power = parseFloat(
-      extensions.getElementsByTagName("power")[0].textContent || "0"
-    );
-    const tpex = extensions.getElementsByTagName(
-      "gpxtpx:TrackPointExtension"
-    )[0];
-    const hr = parseFloat(
-      tpex.getElementsByTagName("gpxtpx:hr")[0].textContent || "0"
-    );
-    const cad = parseFloat(
-      tpex.getElementsByTagName("gpxtpx:cad")[0].textContent || "0"
-    );
 
-    trackPoints.push({ lat, lon, time, ele, power, hr, cad });
+    try {
+      const lat = parseFloat(trkpt.getAttribute("lat") || "0");
+      const lon = parseFloat(trkpt.getAttribute("lon") || "0");
+
+      const timeString =
+        trkpt.getElementsByTagName("time")[0]?.textContent || null;
+      const time = timeString ? new Date(timeString) : new Date(0);
+
+      const ele = parseFloat(
+        trkpt.getElementsByTagName("ele")[0].textContent || "0"
+      );
+
+      const extensions = trkpt.getElementsByTagName("extensions")[0];
+      const power = parseFloat(
+        extensions.getElementsByTagName("power")[0]?.textContent || "0"
+      );
+
+      const tpex = extensions.getElementsByTagName(
+        "gpxtpx:TrackPointExtension"
+      )[0];
+      const hr = parseFloat(
+        tpex.getElementsByTagName("gpxtpx:hr")[0].textContent || "0"
+      );
+      const cad = parseFloat(
+        tpex.getElementsByTagName("gpxtpx:cad")[0].textContent || "0"
+      );
+
+      trackPoints.push({ lat, lon, time, ele, power, hr, cad });
+    } catch (e) {
+      console.error(e);
+      console.error(trkpt);
+    }
   }
 
   return {
